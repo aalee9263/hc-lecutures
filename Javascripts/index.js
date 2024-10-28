e7e10bad18424c3eccbe2c95f614145db4c5ea59
Arshad Ali <aalee9263@gmail.com>
2024-10-28 22:25:49 +0500

    data types and time stamp at 1:01:32

--- Javascripts/index.js
+++ Javascripts/index.js
@@ -1,3 +1,48 @@
-/*28 October 2024
+/*
+
+why to use javascript 
+javascript uses for frontened and backened development
 
 */
+const accoutId = 444553
+let accountEmail = "wWJQ9@example.com"
+var accountPassword = "12345"
+accountCity = "ahmedpur"
+let accountState;
+
+console.log("accountId");
+console.table([accoutId, accountEmail, accountPassword, accountCity, accountState])
+
+//  {
+//     this curly brackets called scope
+//  }
+
+/*
+Prefer not to use var
+because of issue in block scope and functional scope
+*/
+
+"use strict"; // treat all JS code as newer version. 8,9 purani js aur abhi wali me bohat farq hai. to abhi wali ko use kerny k liye use strict ka use kerty hain .
+
+// alert( 3 + 3) // it works only on browser and we are using node.js
+
+///data types
+let name = "Arshad"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof Symbol); // function
+
